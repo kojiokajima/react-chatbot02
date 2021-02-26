eb86427348bad5dd5bb3916092b83bc85f378225
wanko <kouji124@w4.dion.ne.jp>
2021-02-26 14:29:14 -0800

    connected to database

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import defaultDataset from './dataset'
 import './assets/styles/style.css'
 import {AnswersList, Chats} from './components/index'
 import FormDialog from './components/Forms/FormDialog'
+import {db} from './firebase/index'
 
 class App extends React.Component {
   constructor(props) {
@@ -11,7 +11,7 @@ class App extends React.Component {
       answers: [],
       chats: [],
       currentId: "init",
-      dataset: defaultDataset,
+      dataset: {},
       open: false
     }
     this.selectAnswer = this.selectAnswer.bind(this)
@@ -86,9 +86,27 @@ class App extends React.Component {
     })
   };
 
+  initDataset = (dataset) => {
+    this.setState({dataset: dataset});
+  }
+
   componentDidMount() {
-    const initAnswer = ''
-    this.selectAnswer(initAnswer, this.state.currentId)
+    (async() => {
+      const dataset = this.state.dataset
+
+      await db.collection('questions').get().then(snapshots => {
+        snapshots.forEach(doc => {
+          const id = doc.id
+          const data = doc.data()
+          dataset[id] = data
+        })
+      })
+
+      this.initDataset(dataset)
+      const initAnswer = ''
+      this.selectAnswer(initAnswer, this.state.currentId)
+    })();
+
   }
 
   componentDidUpdate(prevProps, prevState, snapshot) {
